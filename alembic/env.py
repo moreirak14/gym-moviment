863d2bc278f0dd93eddd89d2ef17a198c7f033ef
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-04-25 19:54:25 -0300

    fix: pre-commit

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from config import settings
@@ -73,9 +72,7 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True
+            connection=connection, target_metadata=target_metadata, compare_type=True
         )
 
         with context.begin_transaction():
