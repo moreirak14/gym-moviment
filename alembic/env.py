768d89585536f5b8595ab82fa67989c1e95ce2b8
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-04-25 19:49:39 -0300

    feat: alembic config and database connection

--- alembic/env.py
+++ alembic/env.py
@@ -5,22 +5,25 @@ from sqlalchemy import pool
 
 from alembic import context
 from config import settings
-from src.a
+from src.adapters.orm import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option("sqlalchemy.url", settings.database_uri)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -59,15 +62,20 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = settings.database_uri
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True
         )
 
         with context.begin_transaction():
--- config.py
+++ config.py
@@ -10,3 +10,17 @@ settings = Dynaconf(
     settings_files=["settings.toml", ".secret.toml"],
     includes=[f"{PATH_ROOT}/settings.toml", f"{PATH_ROOT}/.secret.toml"],
 )
+
+
+def database_uri():
+    _database_uri = (
+        f"{settings.database_dialect_driver}://"
+        f"{settings.database_user}:{settings.database_password}@"
+        f"{settings.database_host}:{settings.database_port}/"
+        f"{settings.database_name}"
+    )
+
+    return _database_uri
+
+
+settings.database_uri = database_uri()
